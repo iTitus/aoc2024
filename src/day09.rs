f74109e8f0eee5cc80b3500a49174d667bea3d80
iTitus <iTitus@users.noreply.github.com>
2024-12-09 17:20:46 +0100

    cleanup day09

--- src/day09.rs
+++ src/day09.rs
@@ -8,102 +8,112 @@ pub enum Disk {
 
 #[aoc_generator(day9)]
 pub fn input_generator(input: &str) -> Vec<Disk> {
-    let mut disk = vec![];
-    let mut file = true;
-    let mut id = 0;
-    for c in input.chars() {
-        let length = c.to_digit(10).unwrap();
-        let d = if file {
-            let d = Disk::File { id, length };
-            id += 1;
-            d
-        } else {
-            Disk::Free { length }
-        };
-        file = !file;
-        disk.push(d);
-    }
-
-    disk
+    input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap())
+        .enumerate()
+        .map(|(i, length)| {
+            if i % 2 == 0 {
+                Disk::File {
+                    id: i as u32 / 2,
+                    length,
+                }
+            } else {
+                Disk::Free { length }
+            }
+        })
+        .collect()
 }
 
 fn compact_disk<const PART2: bool>(disk_map: &[Disk]) -> Vec<Disk> {
+    println!("compact_disk");
     let mut compact = disk_map.to_vec();
-    if compact.len() > 1 {
-        let mut i = compact.len() - 1;
-        while i > 0 {
-            let mut last = compact[i];
-            match &mut last {
-                Disk::File { id, length } if *length > 0 => {
-                    let mut j = 0;
-                    while *length > 0 && j < i {
-                        match &mut compact[j] {
-                            Disk::Free {
-                                length: free_length,
-                            } if if PART2 {
-                                *free_length >= *length
-                            } else {
-                                *free_length > 0
-                            } =>
-                            {
-                                let how_much = (*length).min(*free_length);
-                                assert!(how_much > 0);
-                                *length -= how_much;
-                                *free_length -= how_much;
+    if compact.len() <= 1 {
+        return compact;
+    }
 
-                                let new_block = Disk::File {
-                                    id: *id,
-                                    length: how_much,
-                                };
-                                if *free_length == 0 {
-                                    compact[j] = new_block;
-                                } else {
-                                    compact.insert(j, new_block);
-                                    j += 1;
-                                    i += 1;
-                                }
-                                compact.insert(i + 1, Disk::Free { length: how_much });
-                            }
-                            _ => {}
-                        }
+    let mut i = compact.len() - 1;
+    while i > 0 {
+        let Disk::File {
+            id,
+            length: mut file_length,
+        } = compact[i]
+        else {
+            i -= 1;
+            continue;
+        };
+        if file_length == 0 {
+            i -= 1;
+            continue;
+        }
 
-                        j += 1;
-                    }
-
-                    if *length == 0 {
-                        compact.remove(i);
-                    } else {
-                        compact[i] = last;
-                    }
-                }
-                _ => {}
+        let mut j = 0;
+        while j < i {
+            let Disk::Free {
+                length: free_length,
+            } = &mut compact[j]
+            else {
+                j += 1;
+                continue;
+            };
+            if (PART2 && *free_length < file_length) || *free_length == 0 {
+                j += 1;
+                continue;
             }
 
-            i -= 1;
+            let blocks = file_length.min(*free_length);
+            assert!(blocks > 0);
+            file_length -= blocks;
+            *free_length -= blocks;
+
+            let new_block = Disk::File { id, length: blocks };
+            if *free_length == 0 {
+                compact[j] = new_block;
+            } else {
+                compact.insert(j, new_block);
+                j += 1;
+                i += 1;
+            }
+
+            if file_length == 0 {
+                compact[i] = Disk::Free { length: blocks };
+                break;
+            } else if let Some(Disk::Free { length }) = compact.get_mut(i + 1) {
+                *length += blocks;
+            } else {
+                compact.insert(i + 1, Disk::Free { length: blocks });
+            }
+
+            j += 1;
         }
+
+        if file_length > 0 {
+            compact[i] = Disk::File {
+                id,
+                length: file_length,
+            };
+        }
+
+        i -= 1;
     }
 
     compact
 }
 
 fn checksum(disk_map: &[Disk]) -> u64 {
-    let mut checksum = 0;
-    let mut i = 0;
-    for d in disk_map {
-        match d {
-            Disk::File { id, length } => {
-                for _ in 0..*length {
-                    checksum += i * (*id as u64);
-                    i += 1;
-                }
-            }
-            Disk::Free { length } => {
-                i += *length as u64;
-            }
-        }
-    }
-
-    checksum
+    disk_map
+        .iter()
+        .flat_map(|d| {
+            let (length, id) = match d {
+                Disk::File { id, length } => (*length as u64, *id as u64),
+                Disk::Free { length } => (*length as u64, 0),
+            };
+            (0..length).map(move |_| id)
+        })
+        .enumerate()
+        .map(|(i, id)| i as u64 * id)
+        .sum()
 }
 
 #[aoc(day9, part1)]
